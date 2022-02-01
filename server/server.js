4ad12420cba6adca1e20f0ba91bf90ceb4c895b0
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-01 12:26:46 +0530

    :zap: added morgan and restaurant router middlewares

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
+const restaurantRouter = require('./routes/restaurants')
 
 const app = express()
+
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+app.use('/api/v1', restaurantRouter)
 
 const port = process.env.PORT || 9008
 
