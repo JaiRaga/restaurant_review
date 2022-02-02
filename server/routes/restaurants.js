b25383c9d711f6756b53298711a2379a5f2a2a54
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-02 09:21:12 +0530

    :sparkles: wrote sql commands for creating and reading restaurants from db

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -4,24 +4,52 @@ const router = express.Router()
 
 // Get all Restaurants
 router.get('/restaurants', async (req, res) => {
-	const result = await db.query('SELECT * FROM restaurants')
-	console.log(result)
-	res
-		.status(200)
-		.send({ status: 'Success', data: { restaurants: ['ihop', 'wendys'] } })
+	try {
+		const results = await db.query('SELECT * FROM restaurants')
+		console.log(results)
+		res.status(200).send({
+			status: 'Success',
+			results: results.rows.length,
+			data: { restaurants: results['rows'] },
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Internal server error')
+	}
 })
 
 // Get Restaurant by id
-router.get('/restaurants/:id', (req, res) => {
-	res
-		.status(200)
-		.send({ status: 'Success', data: { restaurants: ['ihop', 'wendys'] } })
+router.get('/restaurants/:id', async (req, res) => {
+	try {
+		const results = await db.query('SELECT * FROM restaurants where id = $1', [
+			req.params.id,
+		])
+		res
+			.status(200)
+			.send({ status: 'Success', data: { restaurant: results.rows[0] } })
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Internal server error')
+	}
 })
 
 // Create a Restaurant
-router.post('/restaurants', (req, res) => {
-	console.log(req.body)
-	res.status(201).send({ status: 'Success', data: { restaurant: 'something' } })
+router.post('/restaurants', async (req, res) => {
+	try {
+		console.log(req.body)
+		const { name, location, price_range } = req.body
+		const results = await db.query(
+			'INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *',
+			[name, location, price_range]
+		)
+		console.log(results)
+		res
+			.status(201)
+			.send({ status: 'Success', data: { restaurant: results.rows[0] } })
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Internal server error')
+	}
 })
 
 // Update a Restaurant
